e4be0d8a07c32f0c268d3d262c07765d79252dc1
Edgar Jr. San Martin <esanmartinjr@fordham.edu>
2018-10-01 03:38:09 -0400

    Update numberGuess.js

--- numberGuess.js
+++ numberGuess.js
@@ -1,5 +1,5 @@
 /*
-	Write a JavaScript program where the program takes a random integer 
+  Write a JavaScript program where the program takes a random integer 
   between 1 to 10, the user is then prompted to input a guess number.
   If the user input matches with guess number, the program will display
   a message "Good Work" otherwise display a message "Not matched". 
