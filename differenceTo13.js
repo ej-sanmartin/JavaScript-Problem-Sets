669aaf9d26ac61a925ad4c1dc79f82071c2e61b7
Edgar Jr. San Martin <esanmartinjr@fordham.edu>
2018-10-04 04:24:27 -0400

    Create differenceTo13.js

---
+++ differenceTo13.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program to get the difference between a given number and 13,
+// if the number is greater than 13 return double the absolute difference.
+
+function differenceTo13(num){
+  (num <= 13) ? console.log(13 - n) : console.log((num - 13) * 2);
+}
+
+differenceTo13(15);
